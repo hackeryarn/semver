80cce343157c278d69db86b25074fc8194b6f9fd
hackeryarn <artemchernyak@gmail.com>
2019-08-30 17:11:52 -0500

    parsing simple semver

--- src/Semver.hs
+++ src/Semver.hs
@@ -1,6 +1,49 @@
 module Semver
-       ( someFunc
-       ) where
+  ( SemVer (..)
+  , parseSemVer
+  , someFunc
+  ) where
+
+import Control.Applicative
+import Text.Trifecta
 
 someFunc :: IO ()
 someFunc = putStrLn ("someFunc" :: String)
+
+data NumberOrString
+ = NOSS String
+ | NOSI Integer
+ deriving (Eq, Show)
+
+type Major = Integer
+type Minor = Integer
+type Patch = Integer
+type Release = [NumberOrString]
+type Metadata = [NumberOrString]
+
+data SemVer =
+  SemVer Major Minor Patch Release Metadata
+  deriving (Eq, Show)
+
+parseMajor :: Parser Major
+parseMajor =  read <$> (some digit <* char '.')
+
+parseMinor :: Parser Minor
+parseMinor =  read <$> (some digit <* char '.')
+
+parsePatch :: Parser Patch
+parsePatch =  read <$> some digit
+
+parseNumberOrString :: Parser NumberOrString
+parseNumberOrString = try (NOSI . read <$> some digit) <|> (NOSS <$> some alphaNum)
+
+parseRelease :: Parser Release
+parseRelease = many parseNumberOrString
+
+parseMetadata :: Parser Metadata
+parseMetadata = many parseNumberOrString
+
+parseSemVer :: Parser SemVer
+parseSemVer =
+  SemVer <$> parseMajor <*> parseMinor <*> parsePatch <*> parseRelease <*>
+  parseMetadata
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,19 @@
 module Main (main) where
 
+import Test.Hspec
+import Text.Trifecta
+import Semver
+
+maybeSuccess :: Result a -> Maybe a
+maybeSuccess (Success a) = Just a
+maybeSuccess _ = Nothing
+
 main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: String)
+main =
+  hspec $ do
+    let ps = parseString
+        psv = ps parseSemVer mempty
+    describe "Simple semver" $
+      it "parses a simple semver" $ do
+        let r = maybeSuccess $ psv "2.1.1"
+        r `shouldBe` Just (SemVer 2 1 1 [] [])
